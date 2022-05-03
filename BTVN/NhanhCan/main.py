62ebd5e5228c26143e4c70262a21324412f56281
ptatien0307 <79583501+ptatien0307@users.noreply.github.com>
2022-05-03 21:49:06 +0700

    Update main.py

--- BTVN/NhanhCan/main.py
+++ BTVN/NhanhCan/main.py
@@ -1,21 +1,33 @@
+from sys import stdin
 
-def subsetSum(arr, cur_sum, i, sum, count, n):
-     
-    if cur_sum == sum:
-        count[0] += 1
+Count = 0
+Temp = []
+BestSolution = []
+
+
+def BranchAndBound (nums, idx, MaxWeight, n):
+    global Temp
+    global Count
+    global BestSolution
+    if sum(Temp) > MaxWeight:
         return
+    for i in range(idx, len(nums)):
+        Temp.append(nums[i])
+        if len(Temp) == n:
+            if MaxWeight - sum(Temp) >= 0:
+                Count += 1
+                if sum(Temp) > sum(BestSolution):
+                    BestSolution = Temp[:]
+        else:
+            BranchAndBound(nums, i + 1, MaxWeight, n)
+        Temp.pop()
 
-    elif cur_sum == sum or i == n:
-        return
-    else:
-        subsetSum(arr, cur_sum + arr[i], i + 1, sum, count, n)
 
-        subsetSum(arr, cur_sum, i + 1, sum, count, n)
-apples = [int(x) for x in input().split()]
-
-max_mass, n = [int(x) for x in input().split()]
-
-count = [0]
-subsetSum(arr, cur_sum, 0, sum, count, n)
-print(count[0])
-
+if __name__ == '__main__':
+    nums = [int(i) for i in stdin.readline().split()]
+    MaxWeight, n = [int(i) for i in stdin.readline().split()]
+    BranchAndBound(nums, 0,  MaxWeight, n)
+    print(Count)
+    if Count != 0:
+        print(sum(BestSolution))
+        print(BestSolution)
